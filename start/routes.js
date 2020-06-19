706400330e18c770190c5ebbd1ef32869a1a367f
felipebdev <felipebdev@gmail.com>
2020-06-19 15:31:58 -0300

    brainweb teste corrections

--- start/routes.js
+++ start/routes.js
@@ -1,11 +1,10 @@
 const pizzaData = require("../mocked/pizza")
-const pizza = require("../mocked/pizza")
 
 module.exports = (app)=>{
     app.get('/pizzas',(req,res) => res.send(pizzaData))
     app.post('/pizza/make', (req,res) =>{
         const {flavor} = req.body
-        if(flavor === pizza.sugestao.sabor)
+        if(flavor === pizzaData.sugestao.sabor)
             res.send({receivedPoints:true,points:pizzaData.sugestao.pontos,pizzaMounted:req.body})
         else res.send({receivedPoints:false,pizzaMounted:req.body})
     })
